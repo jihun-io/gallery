d94b27d19f9f66dd3d1902cd7c0d0925144b6f57
김지훈 <kim@jihun.io>
2024-10-10 13:59:41 +0900

    다양한 이미지 비율 대응

--- src/components/optimized-image.js
+++ src/components/optimized-image.js
@@ -1,16 +1,7 @@
 "use client";
-
-import Image from "next/image";
 import { getSrcSet } from "../../utils/imageUtils";
 
-const imageLoader = ({ fileName, width }) => {
-  return `/photos/${fileName}-${width}.webp`;
-};
-
-const OptimizedImage = ({ source, fileName, alt, sizes, style, className }) => {
-  const imageSizes = [320, 640, 1024, 1920];
-
-  const srcSet = getSrcSet(fileName, imageSizes);
+const OptimizedImage = ({ source, alt, sizes, style, className }) => {
   return (
     <img
       src={`/photos/originals/${source}`}
@@ -18,7 +9,7 @@ const OptimizedImage = ({ source, fileName, alt, sizes, style, className }) => {
       sizes={sizes}
       className={className}
       style={style}
-      srcSet={srcSet}
+      srcSet={getSrcSet(source)}
       loading="lazy"
     />
   );
